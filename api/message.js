dc4f102688a048a58e9622f95975d85c79547405
Darrickisar <118645566+Darrickisar@users.noreply.github.com>
2025-03-27 23:20:12 +0800

    Update message.js

--- api/message.js
+++ api/message.js
@@ -2,16 +2,16 @@
 import { getData, saveData } from './_data';
 
 export default async function handler(req, res) {
-  if(req.method === 'POST'){
+  if (req.method === 'POST') {
     const { message } = req.body;
-    if(!message){
+    if (!message) {
       return res.status(400).json({ error: '留言内容不能为空' });
     }
     const data = await getData();
     data.messages.push(message);
     await saveData(data);
     res.status(200).json({ messages: data.messages });
-  } else if(req.method === 'GET'){
+  } else if (req.method === 'GET') {
     const data = await getData();
     res.status(200).json({ messages: data.messages });
   } else {
