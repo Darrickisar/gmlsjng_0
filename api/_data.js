b0adfd46f0526ccd8b9bd3726156d70f441d7317
Darrickisar <118645566+Darrickisar@users.noreply.github.com>
2025-03-27 23:18:51 +0800

    Update _data.js

--- api/_data.js
+++ api/_data.js
@@ -7,7 +7,7 @@ export async function getData() {
   let data = await getBlob(DATA_KEY, "vercel_blob_rw_XUNFJUgwGuo8v8MC_ETEualt4YVHQJOGCiI9N53Qdhkqj2G");
   if (!data) {
     data = { messages: [], candleCount: 0, flowerCount: 0, incenseCount: 0 };
-    await putBlob(DATA_KEY, data, process.env.BLOB_READ_WRITE_TOKEN);
+    await putBlob(DATA_KEY, data, "vercel_blob_rw_XUNFJUgwGuo8v8MC_ETEualt4YVHQJOGCiI9N53Qdhkqj2G");
   }
   return data;
 }
