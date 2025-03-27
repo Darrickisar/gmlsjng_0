acf2eea9a146941dc6260bd614e1809aecd171c9
Darrickisar <118645566+Darrickisar@users.noreply.github.com>
2025-03-27 23:00:00 +0800

    Create data.js

---
+++ api/data.js
@@ -0,0 +1,11 @@
+// /api/data.js
+import { getData } from './_data';
+
+export default async function handler(req, res) {
+  if(req.method === 'GET'){
+    const data = await getData();
+    res.status(200).json(data);
+  } else {
+    res.status(405).end(); // 只支持 GET 请求
+  }
+}
